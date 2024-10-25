c129d86c689c28aa38329ce484cb6bca03b1e72b
MrRox1337 <amanrox97@gmail.com>
2024-10-25 17:30:26 +0400

    Added title rename and special animation

---
+++ js/animation.js
@@ -0,0 +1,35 @@
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval = null;
+
+console.log(document.querySelector("h1").innerText);
+
+const siteName = document.getElementById("sitename");
+if (siteName) {
+	console.log("Element selected:", siteName);
+	siteName.onmouseover = (event) => {
+		console.log("Data-value attribute:", event.target.dataset.value); // Check if data-value is accessible
+		let iteration = 0;
+		clearInterval(interval);
+
+		interval = setInterval(() => {
+			event.target.innerText = event.target.innerText
+				.split("")
+				.map((letter, index) => {
+					if (index < iteration) {
+						return event.target.dataset.value[index];
+					}
+					return letters[Math.floor(Math.random() * 26)];
+				})
+				.join("");
+
+			if (iteration >= event.target.dataset.value.length) {
+				clearInterval(interval);
+			}
+
+			iteration += 1 / 3;
+		}, 30);
+	};
+} else {
+	console.log("Element not found.");
+}
