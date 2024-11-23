8b11cd070807bceb277ef8492d3c06fd30f4df62
Aman Mishra <amanrox97@gmail.com>
2024-11-23 20:25:41 +0400

    Increased animation speed

--- js/animation.js
+++ js/animation.js
@@ -6,30 +6,30 @@ console.log(document.querySelector("h1").innerText);
 
 const siteName = document.getElementById("sitename");
 if (siteName) {
-	console.log("Element selected:", siteName);
-	siteName.onmouseover = (event) => {
-		console.log("Data-value attribute:", event.target.dataset.value); // Check if data-value is accessible
-		let iteration = 0;
-		clearInterval(interval);
+    console.log("Element selected:", siteName);
+    siteName.onmouseover = (event) => {
+        console.log("Data-value attribute:", event.target.dataset.value); // Check if data-value is accessible
+        let iteration = 0;
+        clearInterval(interval);
 
-		interval = setInterval(() => {
-			event.target.innerText = event.target.innerText
-				.split("")
-				.map((letter, index) => {
-					if (index < iteration) {
-						return event.target.dataset.value[index];
-					}
-					return letters[Math.floor(Math.random() * 26)];
-				})
-				.join("");
+        interval = setInterval(() => {
+            event.target.innerText = event.target.innerText
+                .split("")
+                .map((letter, index) => {
+                    if (index < iteration) {
+                        return event.target.dataset.value[index];
+                    }
+                    return letters[Math.floor(Math.random() * 26)];
+                })
+                .join("");
 
-			if (iteration >= event.target.dataset.value.length) {
-				clearInterval(interval);
-			}
+            if (iteration >= event.target.dataset.value.length) {
+                clearInterval(interval);
+            }
 
-			iteration += 1 / 3;
-		}, 30);
-	};
+            iteration += 1 / 1.5;
+        }, 30);
+    };
 } else {
-	console.log("Element not found.");
+    console.log("Element not found.");
 }
